a69b15b4e33d3d3829308e190a888a3370cc8ea4
emilienaufauvre <emilien.aufauvre@live.fr>
2025-12-28 00:11:55 +0100

    docs: add local image example

---
+++ example/src/app/2.-Marker/2.7.-Use-a-local-image.tsx
@@ -0,0 +1,68 @@
+import {
+  Map,
+  MapProvider,
+  Marker,
+  type MarkerRef,
+  useLocalImage,
+} from 'react-native-maplibre-gl-js'
+import { useRef } from 'react'
+
+/**
+ * @returns - Example of the marker component usage, with an image
+ *  stored locally in the app bundle, instead of a remote URL.
+ */
+const Screen = () => {
+  // Refs.
+  const markerRef = useRef<MarkerRef | null>(null)
+  // Load the local image in base64 using the "useLocalImage" hook.
+  const image = useLocalImage(require('../../../assets/marker.png'))
+
+  return (
+    <MapProvider>
+      <Map
+        options={{
+          style: 'https://tiles.openfreemap.org/styles/liberty',
+          center: [2.32, 48.86],
+          zoom: 12,
+        }}
+      />
+      <Marker
+        ref={markerRef}
+        options={{
+          draggable: true,
+          element: {
+            // The image is then used with the <img/> tag.
+            innerHTML: `
+              <style>
+                .pin {
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  margin: 0;
+                  border-radius: 50%;
+                  background-color: #FFF;
+                  box-shadow: 0 0 10px #000A;
+                }
+                .pin img {
+                  width: 64px;
+                  height: 64px;
+                  object-fit: contain;
+                }
+              </style>
+              <div class="pin">
+                <img alt="pin" src="${image}" />
+              </div>
+            `,
+          },
+        }}
+        listeners={{
+          mount: {
+            rnListener: () => markerRef.current?.setLngLat([2.32, 48.86]),
+          },
+        }}
+      />
+    </MapProvider>
+  )
+}
+
+export default Screen
