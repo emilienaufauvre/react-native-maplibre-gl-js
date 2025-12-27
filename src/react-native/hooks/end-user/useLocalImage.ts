c34ee0ef253fd2e0767ea8b680877c57a2266f45
emilienaufauvre <emilien.aufauvre@live.fr>
2025-12-28 00:26:27 +0100

    docs: update useLocalImage docs

--- src/react-native/hooks/end-user/useLocalImage.ts
+++ src/react-native/hooks/end-user/useLocalImage.ts
@@ -2,13 +2,24 @@ import { useEffect, useState } from 'react'
 import { assetToDataUri } from './useLocalImage.utils'
 
 /**
- * Load a local image as base64 data URI, and return it.
+ * Load a local image as base64 data URI, and return it. It can then be used
+ * in HTML elements.
  * @param moduleId - The module ID of the image to load (e.g.
  *  `require('./image.png')`).
  * @returns - The base64 data URI of the image.
  * @example
  * ```tsx
  * const image = useLocalImage(require('./image.png'))
+ *
+ * ...
+ *
+ * <Marker
+ *   options={{
+ *     element: {
+ *       innerHTML: `<img alt="pin" src="${image}" />`
+ *     },
+ *   }}
+ * />
  * ```
  * @group Hooks
  */
